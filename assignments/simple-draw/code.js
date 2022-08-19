dfcf6e86648712ac19ce88e61d329f7048e0daeb
AvaTheEpicGamer <111394749+AvaTheEpicGamer@users.noreply.github.com>
2022-08-19 15:14:59 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -41,7 +41,7 @@ drawFilledRect(0, 0, 1000, 1000, 'white');
 drawFilledCircle(250, 200, 130, 'black');
 drawFilledRect(120, 190, 260, 290, 'black');
 drawFilledRect(175, 250, 255, 150, 'black');
-drawFilledRect(285, 480, 65, 55, 'black');
+drawFilledRect(285, 480, 65, 60, 'black');
 
 
 drawFilledRect(125, 190, 250, 290, 'red');
