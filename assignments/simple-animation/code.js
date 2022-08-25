1356fbedd3e20f0669885b2a41e9abf2bfb38f83
AvaTheEpicGamer <111394749+AvaTheEpicGamer@users.noreply.github.com>
2022-08-25 15:07:47 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,7 +6,7 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-  drawFilledCircle((time / 1) % width, height / 2, 255, 'red');
+  drawFilledCircle((time / .5) % width, height / 2, 255, 'red');
   drawFallingTriangle(width / 2, time);
 };
 
